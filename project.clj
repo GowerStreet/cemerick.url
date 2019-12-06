97939fdce50179d433bf429785d4f88da5354e1c
Henry Widd <henryw374@gmail.com>
2019-12-06 11:09:47 +0000

    bump pathetic

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [pathetic "0.5.0"]
+                 [pathetic "0.5.1"]
                  [org.clojure/clojurescript "1.10.238" :optional true]]
 
   :source-paths ["src" "target/generated-src"]
